b976ef4dda2cdc6dc466bff0d6e5e7b7124bb85c
hjklog <hjklog98@likelion.org>
2023-05-25 04:21:58 +0900

    main font change

--- src/Home.js
+++ src/Home.js
@@ -18,8 +18,8 @@ function Home() {
             <source src={`video/background-video.mp4`} type='video/mp4' />
           </video>
           <div>
-            <div className="glitch" data-glitch="POOL:US">POOL:US</div>
-            <div className="text-white my-3 text-center sub-title">2023 영암축전<br /> 2023.05.30 - 2023.05.31</div>
+            <div className="glitch anton-font" data-glitch="POOL:US">POOL:US</div>
+            <div className="text-white my-3 text-center sub-title isamanroo-font">2023 영암축전<br /> 2023.05.30 - 2023.05.31</div>
           </div>
         </div>
       </div>
