20cf406d58f08fb843a4a201a6e953cff8e04d6d
hjklog <hjklog98@likelion.org>
2023-05-27 01:20:22 +0900

    아티스트 라인업 클릭 화면 수정

--- src/Artist/Lineuptitle.js
+++ src/Artist/Lineuptitle.js
@@ -1,18 +1,32 @@
 import React from 'react'
-import { TbClick } from "react-icons/tb";
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import './Artist.css';
 
 function Lineuptitle() {
   return (
+    <>
+    <Helmet>
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    </Helmet>
     <section className="display-container">
         <div className="Lineuptitle good">       
             <Link to="/ArtistLineup" className="clickparent">
-                <TbClick className="click" size="30" color="white" />
+            <div className='click'>
+              <lottie-player
+                src="https://assets7.lottiefiles.com/packages/lf20_vykqp3ao.json"
+                background="transparent"
+                speed="1"
+                style={{ width: '300px', height: '300px' }}
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
                 <img src="img/artist/artistTitle.png" alt="라인업메인로고" />
             </Link>
         </div>
     </section>
+    </>
   )
 }
 
