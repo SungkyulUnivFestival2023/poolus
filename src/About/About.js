b26214c40fe2b81ecab6b4953a2ad7e1a8aae72f
hjklog <hjklog98@likelion.org>
2023-05-27 19:50:30 +0900

    about 코드 정리

--- src/About/About.js
+++ src/About/About.js
@@ -17,30 +17,18 @@ function About() {
                 {/* <div className="pooluslogo"><img src="img/logo/heartlogo.png" alt="정식로고" /></div> */}
                 {/* <div className="pooluslogo1"><img src="img/logo/waterpoolus.png" alt="로고글씨" /></div> */}
                 <div className="pooluslogo2 ">만든이들</div>
-                
-                {/* <div className="logoX">
-                    <div>
-                        <img className='likelion-logo' src="img/logo/likelion.png" alt="멋사로고" />
-                        <span class="llblue">성결대학교<br/>멋쟁이사자처럼 11기</span>
-                    </div>
-                    <span><FaHandshake /></span>
-                    <div>
-                        <img className='renew-logo' src="img/logo/renewlogo.png" alt="리뉴로고" />
-                        <span className="renewyellow">RE:NEW<br/>총학생회</span>
-                    </div>
-                </div> */}
                 <div className="logoX">
                     <div className='sku-likelion'>
                         <img className='likelion-logo' src="img/logo/likelion.png" alt="멋사로고" />
                         <span class="llblue">성결대학교<br/>멋쟁이사자처럼 11기</span>
                     </div>
+                    {/* <span><FaHandshake /></span> */}
                     <div className='sku-renew'>
                         <img className='renew-logo' src="img/logo/renewlogo.png" alt="리뉴로고" />
                         <span className="renewyellow">RE:NEW<br/>총학생회</span>
                     </div>
                     <Lottie animationData={animationData} style={{width: '200px', height: '200px'}}/>
                 </div>
-
                 <p className="collaboration">
                     성결대학교 제39대 RE:NEW 총학생회에서 축제를 기획하였습니다.<br />
                     성결대학교 멋쟁이사자처럼 대학 11기에서 웹 사이트를 제작하였습니다.
